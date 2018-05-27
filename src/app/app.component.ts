ba3d7fa88c18b9b83d0edb564f2762bbddaab435
jhraga <jhraga@gmail.com>
2018-05-27 10:33:51 -0400

    Nav bar and responsive nice view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
