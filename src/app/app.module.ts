9f5cb7a05c06d7fa2cc77c1ada5b854c0138df8e
jhraga <jhraga@gmail.com>
2018-05-31 07:07:06 -0400

    Funcionalidad sin dificultades del menu, cambio de estilos y contenido de la página EspiritiCima para su creación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
