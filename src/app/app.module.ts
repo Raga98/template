bc42b391e1a681900cc2d601b906a2bd87203abf
jhraga <jhraga@gmail.com>
2018-06-01 10:02:51 -0400

    Menu Drops, Admin login y route funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,35 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { ContentComponent } from './content/content.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
 
+
+
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent},
-  { path: 'admin', component: AdminComponent},
+  { path: 'content', component: ContentComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
   { path: '', redirectTo: '/content', pathMatch: 'full' },
   { path: '**', redirectTo: '/content', pathMatch: 'full' },
 ];
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
     ContentComponent,
     LoginComponent,
     AdminComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+    /*  { enableTracing: true } // <-- debugging purposes only*/
+    ),
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
