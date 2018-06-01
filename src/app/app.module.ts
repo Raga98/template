0bc4c241439efb164b3d9ed3c010452b5cf1f2d6
Jeremy Raga <ragajeremy@gmail.com>
2018-05-31 20:26:05 -0400

    Generación del componente login y admni, problemas con el router outlet para la navegación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContentComponent } from './content/content.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'admin', component: AdminComponent},
+  { path: '', redirectTo: '/content', pathMatch: 'full' },
+  { path: '**', redirectTo: '/content', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    ContentComponent
+    ContentComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
