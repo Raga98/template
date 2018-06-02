eb281dc45ccc6b0b27f58f0223cb0733899dbec0
Oscar <oscar.3131@hotmail.com>
2018-06-01 21:30:11 -0400

    lul

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDagli8Ga2k2qme33GZo4PYS-Gp6Pm3GgU',
+    authDomain: 'espiritacima.firebaseapp.com',
+    databaseURL: 'https://espiritacima.firebaseio.com',
+    projectId: 'espiritacima',
+    storageBucket: '',
+    messagingSenderId: '111160775127'
+  }
 };
