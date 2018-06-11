7cce8f9aa1e564b1cff95fc999a6f0437d8db413
JeremyRaga <ragajeremy@gmail.com>
2018-06-10 20:30:37 -0400

    Nada nuevo, cambio de los servicios del admincomponent hacia los compoenentes hijos

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,46 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Router, CanActivate, CanActivateChild} from '@angular/router';
-import { User } from '../models/user';
-import { Pubs } from '../models/pubs';
-import { PubsService } from '../services/pubs.service';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
+
 export class AdminComponent implements OnInit {
 
-user = {} as User;
-pubs = {} as Pubs;
-authState = null;
-
-  constructor(private afAuth: AngularFireAuth, public router: Router, private pubsService: PubsService ) { }
+  constructor(private afAuth: AngularFireAuth, public router: Router) { }
 
   ngOnInit(){
     
   }
-
-  createPub(){
-     this.pubsService.addPub(this.pubs);
-
-  }
-
-  updatePub(){
-    this.pubsService.updatePub(this.pubs);
-  }
-
-  deletePub(){
-    this.pubsService.deletePub(this.pubs);
-  }
-
   adminSignOut(){
     this.afAuth.auth.signOut().then( a => {
       this.router.navigate(['/content']);
     });
-    
   }
-
 
   boton: number
 
--- src/app/articulo-dash/articulo-dash.component.ts
+++ src/app/articulo-dash/articulo-dash.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, CanActivate, CanActivateChild} from '@angular/router';
+import { User } from '../models/user';
+import { Pubs } from '../models/pubs';
+import { PubsService } from '../services/pubs.service';
 
 @Component({
   selector: 'articulo-dash',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticuloDashComponent implements OnInit {
 
-  constructor() { }
+  user = {} as User;
+  pubs = {} as Pubs;
+  authState = null;
+
+
+  constructor(public router: Router, private pubsService: PubsService) { }
 
   ngOnInit() {
+  
   }
 
+  createPub() {
+    this.pubsService.addPub(this.pubs);
+  
+  }
+
+ updatePub() {
+    this.pubsService.updatePub(this.pubs);
+  
+  }
+
+  deletePub() {
+    this.pubsService.deletePub(this.pubs);
+  
+  }
 }
